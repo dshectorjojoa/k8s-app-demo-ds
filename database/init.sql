b3e7d0d85362f69e792d1637e9c377e724b63282
dshectorjoja <ds.hectorjojoa@gmail.com>
2025-01-24 14:49:21 -0500

    Add k8s manifests and pipeline configuration

---
+++ database/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE counter;
+\c counter
+
+CREATE TABLE visits (
+    id SERIAL PRIMARY KEY,
+    count INTEGER DEFAULT 0,
+    environment VARCHAR(50)
+);
+
+INSERT INTO visits (count, environment) VALUES (0, 'develop');
