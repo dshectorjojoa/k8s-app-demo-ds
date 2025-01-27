4838da9e9fabf9f01b27f88fa34c50e0074a23ce
dshectorjoja <ds.hectorjojoa@gmail.com>
2025-01-27 12:16:02 -0500

    hola_mundo_cd_ci

--- database/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE visits (
-    id SERIAL PRIMARY KEY,
-    count INTEGER DEFAULT 0,
-    environment VARCHAR(10) NOT NULL
-);
-
-INSERT INTO visits (count, environment) VALUES (0, 'develop');
